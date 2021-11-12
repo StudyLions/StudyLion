0332d2815472b9ebefda461d6f9ae4cc1503e40d
Harsha Raghu <funnyboy313@gmail.com>
2021-11-12 21:51:34 +0530

    [STUDY] Update handle study sessions
    Update STUDY module to handle study sessions, listening to voice_state_update event

--- bot/modules/study/time_tracker.py
+++ bot/modules/study/time_tracker.py
@@ -10,6 +10,9 @@ from core import Lion
 from .module import module
 from . import admin
 
+from settings import GuildSettings
+from data import NULL, tables
+import datetime
 
 last_scan = {}  # guildid -> timestamp
 
@@ -105,5 +108,87 @@ async def launch_study_tracker(client):
     await admin.untracked_channels.launch_task(client)
     asyncio.create_task(_study_tracker())
 
+# only whitelisted non-bot member
+def member_joined_voice_channel(user, member, joinedchannel):
+
+    # Check if last_study_session_start is unset in data
+    if user.data.last_study_session_start is not None or user.data.session_start_coins is not 0:
+        GuildSettings(member.guild.id).event_log.log(
+            "{} has joined `#{}` but last_study_session_start is {} and session_start_coins is {}".format(
+                member.mention,
+                joinedchannel,
+                user.data.last_study_session_start,
+                user.data.session_start_coins
+            ), title="Member Session Data Inconsistent"
+        )
+
+    # set Start Study session timestamp, coins data
+    user.data.last_study_session_start = datetime.datetime.utcnow()
+    user.data.session_start_coins = user.coins
+
+    GuildSettings(member.guild.id).event_log.log(
+        "{} has joined `#{}`".format(
+                member.mention,
+                joinedchannel,
+            ), title="Member joined Channel"
+    )
+
+# only whitelisted non-bot member
+def member_left_voice_channel(user, member, channelleft):
+
+    last_study_session_start = user.data.last_study_session_start
+    session_start_coins = user.data.session_start_coins
+
+    # reset Start Study session timestamp
+    user.data.last_study_session_start = None
+    user.data.session_start_coins = 0 # Default 0 not None
+
+    now = datetime.datetime.utcnow()
+    
+    # Just show the coin alert when the user had atleast earned 1 coin
+    if (user.coins - session_start_coins) > 0:
+        GuildSettings(member.guild.id).coin_alert_channel.log(
+            "{} has left `#{}` and spent a total time of `{:.2f}` mins and earned reward of `{}` coins".format(
+                    member.mention,
+                    channelleft,
+                    (now - last_study_session_start).total_seconds() / 60,
+                    (user.coins - session_start_coins)
+                ), title="Hourly Reward"
+        )
+    
+    GuildSettings(member.guild.id).event_log.log(
+        "{} has left `#{}` and spent a total time of `{:.2f}` mins".format(
+                member.mention,
+                channelleft,
+                (now - last_study_session_start).total_seconds() / 60
+            ), title="Member Left Channel"
+    )
+
+@client.add_after_event("voice_state_update")
+async def voice_state_log_updater(client, member, before, after):
+    if not client.is_ready():
+        # The poll loop will pick it up
+        return
+
+    if member.bot:
+        return
+    if member.id in client.objects['blacklisted_users']:
+        return
+    if member.id in client.objects['ignored_members'][member.guild.id]:
+        return
+    
+    # Ensure guild row exists
+    tables.guild_config.fetch_or_create(member.guild.id)
+    
+    # Fetch guild user
+    user = Lion.fetch(member.guild.id, member.id)
+
+    if after.channel and not before.channel:
+        # Client joined a voice channel
+        member_joined_voice_channel(user, member, after.channel)
+
+    if before.channel and not after.channel:
+        # client left a Voice Channel
+        member_left_voice_channel(user, member, before.channel)
 
 # TODO: Logout handler, sync
