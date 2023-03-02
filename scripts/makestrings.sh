aa326b759b08d1eac8f7bcca9999bcafec83e0a3
Conatum <cona@thewisewolf.dev>
2023-03-02 19:03:13 +0200

    rewrite (strings): Register new domains.

--- scripts/makestrings.sh
+++ scripts/makestrings.sh
@@ -1,3 +1,7 @@
-xgettext bot/modules/sysadmin/exec_cog.py -o locales/templates/exec.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
-xgettext bot/babel/*.py -o locales/templates/babel.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
-xgettext bot/modules/reminders/*.py -o locales/templates/reminders.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
+xgettext src/modules/sysadmin/exec_cog.py -o locales/templates/exec.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
+xgettext src/babel/*.py -o locales/templates/babel.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
+xgettext src/modules/reminders/*.py -o locales/templates/reminders.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
+xgettext src/gui/cards/stats.py -o locales/templates/stats-gui.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
+xgettext src/gui/cards/weekly.py -o locales/templates/weekly-gui.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
+xgettext src/gui/cards/monthly.py -o locales/templates/monthly-gui.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
+xgettext src/gui/cards/goals.py -o locales/templates/goals-gui.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
