1af12fc1875055a3b7d30c4e1ef3ceb82fc58046
Conatum <cona@thewisewolf.dev>
2023-03-12 15:23:58 +0200

    rewrite: Add empty test setup.

---
+++ src/modules/test/__init__.py
@@ -0,0 +1,9 @@
+
+
+async def setup(bot):
+    # from .test import TestCog
+    # from .data import test_data
+
+    # bot.db.load_registry(test_data)
+    # await bot.add_cog(TestCog(bot))
+    pass
