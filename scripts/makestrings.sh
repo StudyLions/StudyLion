31421408494f2aad1137fdd02b053bdda6957269
Conatum <cona@thewisewolf.dev>
2023-05-25 16:46:12 +0300

    rewrite: Localisation scripts.

--- scripts/makestrings.sh
+++ scripts/makestrings.sh
@@ -1,7 +1,7 @@
-xgettext src/modules/sysadmin/exec_cog.py -o locales/templates/exec.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
-xgettext src/babel/*.py -o locales/templates/babel.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
-xgettext src/modules/reminders/*.py -o locales/templates/reminders.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
-xgettext src/gui/cards/stats.py -o locales/templates/stats-gui.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
-xgettext src/gui/cards/weekly.py -o locales/templates/weekly-gui.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
-xgettext src/gui/cards/monthly.py -o locales/templates/monthly-gui.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
-xgettext src/gui/cards/goals.py -o locales/templates/goals-gui.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3
+#!/bin/bash
+
+for filename in locales/domains/*.txt; do
+  domain=$(basename "$filename" .txt)
+  echo "Creating template for domain: $domain"
+  xgettext -f $filename -o locales/templates/$domain.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3 -v -c --debug
+done
---
+++ scripts/start_leo_debug.py
@@ -0,0 +1,15 @@
+# !/bin/python3
+
+import sys
+import os
+import tracemalloc
+
+tracemalloc.start()
+
+sys.path.insert(0, os.path.join(os.getcwd()))
+sys.path.insert(0, os.path.join(os.getcwd(), "src"))
+
+
+if __name__ == '__main__':
+    from bot import _main
+    _main()
