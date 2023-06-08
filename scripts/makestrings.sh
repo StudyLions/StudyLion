83acfe21e78f0f252ba2c0ced3f6e2f9be2a34cf
Conatum <cona@thewisewolf.dev>
2023-06-08 21:00:43 +0300

    rewrite: Version compatibility for gettext.

--- scripts/makestrings.sh
+++ scripts/makestrings.sh
@@ -3,5 +3,5 @@
 for filename in locales/domains/*.txt; do
   domain=$(basename "$filename" .txt)
   echo "Creating template for domain: $domain"
-  xgettext -f $filename -o locales/templates/$domain.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3 -v -c --debug
+  xgettext -f $filename -o locales/templates/$domain.pot --keyword=_p:1c,2 --keyword=_n:1,2 --keyword=_np:1c,2,3 -c --debug --from-code=utf-8
 done
