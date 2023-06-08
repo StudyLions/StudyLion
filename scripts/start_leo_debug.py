446b3b50856d42a01cf06127ce3f734a59f63074
Conatum <cona@thewisewolf.dev>
2023-06-08 17:31:30 +0300

    rewrite: More thorough debug mode.

--- scripts/start_leo_debug.py
+++ scripts/start_leo_debug.py
@@ -3,11 +3,29 @@
 import sys
 import os
 import tracemalloc
+import asyncio
+import logging
 
-tracemalloc.start()
 
 sys.path.insert(0, os.path.join(os.getcwd()))
 sys.path.insert(0, os.path.join(os.getcwd(), "src"))
+
+tracemalloc.start()
+event_loop = asyncio.get_event_loop()
+
+
+def loop_exception_handler(loop, context):
+    print(context)
+    task: asyncio.Task = context.get('task', None)
+    if task is not None:
+        addendum = f"<Task name='{task.name}' stack='{task.get_stack()}'>"
+        message = context.get('message', '')
+        context['message'] = ' '.join((message, addendum))
+    loop.default_exception_handler(context)
+
+
+event_loop.set_exception_handler(loop_exception_handler)
+event_loop.set_debug(enabled=True)
 
 
 if __name__ == '__main__':
