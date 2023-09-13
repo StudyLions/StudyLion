7b18a7938dca5d2b5917cae6573968d73c58c0fd
Conatum <cona@thewisewolf.dev>
2023-09-14 01:52:23 +0300

    fix(ranks): Correct overview note creation.

--- src/modules/ranks/ui/overview.py
+++ src/modules/ranks/ui/overview.py
@@ -380,34 +380,6 @@ class RankOverviewUI(MessageUI):
             ] or [[]]
             lines = line_blocks[self.pagen]
             desc = '\n'.join(reversed(lines))
-
-            # Add note about season start
-            note_name = t(_p(
-                'ui:rank_overview|embed|field:note|name',
-                "Note"
-            ))
-            season_start = self.lguild.data.season_start
-            if season_start:
-                season_str = t(_p(
-                    'ui:rank_overview|embed|field:note|value:with_season',
-                    "Ranks are determined by activity since {timestamp}."
-                )).format(
-                    timestamp=discord.utils.format_dt(season_start)
-                )
-            else:
-                season_str = t(_p(
-                    'ui:rank_overview|embed|field:note|value:without_season',
-                    "Ranks are determined by *all-time* statistics.\n"
-                    "To reward ranks from a later time (e.g. to have monthly/quarterly/yearly ranks) "
-                    "set the `season_start` with {stats_cmd}"
-                )).format(stats_cmd=self.bot.core.mention_cmd('configure statistics'))
-            if self.rank_type is RankType.VOICE:
-                addendum = t(_p(
-                    'ui:rank_overview|embed|field:note|value|voice_addendum',
-                    "Also note that ranks will only be updated when a member leaves a tracked voice channel! "
-                    "Use the **Refresh Member Ranks** button below to update all members manually."
-                ))
-                season_str = '\n'.join((season_str, addendum))
         else:
             # No ranks, give hints about adding ranks
             desc = t(_p(
@@ -439,6 +411,33 @@ class RankOverviewUI(MessageUI):
             description=desc
         )
         if show_note:
+            # Add note about season start
+            note_name = t(_p(
+                'ui:rank_overview|embed|field:note|name',
+                "Note"
+            ))
+            season_start = self.lguild.data.season_start
+            if season_start:
+                season_str = t(_p(
+                    'ui:rank_overview|embed|field:note|value:with_season',
+                    "Ranks are determined by activity since {timestamp}."
+                )).format(
+                    timestamp=discord.utils.format_dt(season_start)
+                )
+            else:
+                season_str = t(_p(
+                    'ui:rank_overview|embed|field:note|value:without_season',
+                    "Ranks are determined by *all-time* statistics.\n"
+                    "To reward ranks from a later time (e.g. to have monthly/quarterly/yearly ranks) "
+                    "set the `season_start` with {stats_cmd}"
+                )).format(stats_cmd=self.bot.core.mention_cmd('configure statistics'))
+            if self.rank_type is RankType.VOICE:
+                addendum = t(_p(
+                    'ui:rank_overview|embed|field:note|value|voice_addendum',
+                    "Also note that ranks will only be updated when a member leaves a tracked voice channel! "
+                    "Use the **Refresh Member Ranks** button below to update all members manually."
+                ))
+                season_str = '\n'.join((season_str, addendum))
             embed.add_field(
                 name=note_name,
                 value=season_str,
